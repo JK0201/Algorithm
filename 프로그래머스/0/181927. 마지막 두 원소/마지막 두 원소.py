a0055e9b123ddb3bab5ead16aab2faf2c5393763
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-18 02:09:03 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,15 +1,10 @@
 def solution(num_list):
-    answer = []
-    
-    for n in num_list:
-        answer.append(n)
+    answer = num_list
     
     if answer[-2] < answer[-1]:
         answer.append(answer[-1] - answer[-2])
         
     elif answer[-2] >= answer[-1]:
         answer.append(answer[-1] * 2)
-    
-    print(answer)
-    
+
     return answer

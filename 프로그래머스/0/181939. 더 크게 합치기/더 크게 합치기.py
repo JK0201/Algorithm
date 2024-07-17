bcb59935006b515b6fab599cec25a4d1855deb37
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-17 23:49:07 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -2,11 +2,4 @@ def solution(a, b):
     n = int(str(a) + str(b))
     m = int(str(b) + str(a))
     
-    if n > m:
-        return n
-    
-    elif n < m:
-        return m 
-        
-    elif n == m:
-        return n
+    return max(n, m)

027531a1f7139680022773b2493202252cfeeb7e
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-17 18:35:48 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.36 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,11 +1,10 @@
 def solution(my_string, overwrite_string, s):
     answer = ''
-    str = [l for l in my_string]
     
-    for idx, l in enumerate(overwrite_string):
-        str[idx + s] = l
-    
-    for l in str:
-        answer+= l
-    
+    for l in range(len(my_string)):
+        if l >= s and l-s < len(overwrite_string):
+            answer += overwrite_string[l-s]
+        else:
+            answer += my_string[l]
+        
     return answer

08774944cff5565c5db3310255b07a37bbfdb3ff
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-17 23:38:04 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -2,15 +2,8 @@ from collections import deque
 
 def solution(str1, str2):
     answer = ''
-    stack = []
-    s1 = deque(s for s in str1)
-    s2 = deque(s for s in str2)
-
-    while s1 and s2 :
-        stack.append(s1.popleft())
-        stack.append(s2.popleft())
     
-    for s in stack:
-        answer += s
+    for i in range(len(str1)):
+        answer += str1[i] + str2[i]
     
     return answer

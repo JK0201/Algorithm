bfbcb100a0b9e16792666df6e7d22fc1e049960d
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-18 17:29:29 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,23 +2,23 @@ from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    
     q = deque(progresses)
     s = deque(speeds)
     
+    cnt = 0
+    time = 0
+    
     while q:
-        cnt = 0
-        idx = 0
-        while idx < len(q):
-            q[idx] += s[idx]
-            idx += 1
-            
-        while q and q[0] >= 100:
+        if q[0] + s[0] * time >= 100:
             q.popleft()
             s.popleft()
             cnt += 1
-        
-        if cnt != 0:
-            answer.append(cnt)
-        
+            
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            time += 1
+            
+    answer.append(cnt)
     return answer

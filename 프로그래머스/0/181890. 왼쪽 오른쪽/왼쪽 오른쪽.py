3c6eedd7b243a0ef34f810fa13d5bebc06f7a8be
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-20 08:33:49 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -3,11 +3,9 @@ def solution(str_list):
     
     for i in range(len(str_list)):
         if str_list[i] == "l":
-            answer = str_list[:i]
-            break
+            return str_list[:i]
         
         elif str_list[i] == "r":
-            answer = str_list[i+1:]
-            break
+            return str_list[i+1:]
             
-    return answer
+    return []

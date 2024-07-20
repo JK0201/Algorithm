95eb377dae177a1e0faeab87ad2eacdbf795367e
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-20 23:45:39 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,11 @@
+def solution(myString):
+    answer = ''
+    
+    for i in range(len(myString)):
+        if myString[i] == 'a' or myString[i] == 'A':
+            answer += myString[i].upper()
+            
+        else:
+            answer += myString[i].lower()
+    
+    return answer

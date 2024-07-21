9edfc8c74cd2a402587435ffef4b8692476e920d
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-21 22:51:07 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = []
+    
+    num_list.sort()
+    
+    for i, n in enumerate(num_list):
+        if i >= 5:
+            answer.append(n)
+    
+    return answer

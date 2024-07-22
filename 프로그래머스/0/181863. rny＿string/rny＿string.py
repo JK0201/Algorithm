a70b27cafeb098a7a89345e29af339783b83b740
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-22 14:30:15 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,11 @@
+def solution(rny_string):
+    answer = ''
+    
+    for s in rny_string:
+        if s == 'm':
+            answer += 'rn'
+            
+        else:
+            answer += s
+    
+    return answer

768e6771081124f0e5bc05af9d76966a61454a02
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-30 03:19:46 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -2,10 +2,8 @@ def solution(A,B):
     answer = 0
     A.sort()
     B.sort(reverse=True)
-    i = 0
     
-    while i < len(A):
-        answer += A[i] * B[i]
-        i += 1
+    for a, b in zip(A, B):
+        answer += a * b
         
     return answer

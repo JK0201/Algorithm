0efc4002f3dfd479fee9fc3e618c043bd481939e
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-07-31 03:00:21 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,10 +2,11 @@ n = int(input())
 words = str(input())
 alpha_num = {chr(a) : i + 1 for i, a in enumerate(range(ord('a'), ord('z') + 1))}
 res = 0
+mod = 1234567891
 
 for i in range(len(words)):
     v = words[i]
     if v in alpha_num:
         res += alpha_num[v] * (31 ** i)
         
-print(res)
+print(res % mod)

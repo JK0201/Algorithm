67679550c4823c1d79da6825f3f2dcbce35bfead
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-08 15:17:33 +0900

    [level 3] Title: 네트워크, Time: 0.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,26 @@
+from collections import deque
+
 def solution(n, computers):
     number_of_networks = 0
     visited = [False] * n
     
-    def dfs(cur_i):
-        visited[cur_i] = True
-        network = computers[cur_i]
+    def bfs(i):
+        visited[i] = True
+        q = deque()
+        q.append(i)
         
-        for i in range(len(network)):
-            if network[i] == 1 and not visited[i]:
-                dfs(i)
+        while q:
+            cur_i = q.popleft()
+            network = computers[cur_i]
+            
+            for i in range(len(network)):
+                if network[i] == 1 and not visited[i]:
+                    visited[i] = True
+                    q.append(i)
     
     for i in range(n):
         if not visited[i]:
-            dfs(i)
+            bfs(i)
             number_of_networks += 1
             
     return number_of_networks

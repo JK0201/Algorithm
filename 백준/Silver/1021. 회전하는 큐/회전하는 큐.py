a26726f6056042cc6abbf70d4a7fd97cc3b33317
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-14 12:02:32 +0900

    [Silver III] Title: 회전하는 큐, Time: 52 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,5 +1,6 @@
 import sys
 from collections import deque
+
 n, m = map(int, sys.stdin.readline().split())
 target = list(map(int, sys.stdin.readline().split()))
 
@@ -7,25 +8,16 @@ q = deque(i for i in range(1, n+1))
 
 cnt = 0
 for t in target:
-    if q.index(t) <= len(q) - q.index(t):
-        while True:
-            num = q.popleft()
-            if num != t:
-                cnt += 1
-                q.append(num)
-                
-            else:
-                break
-            
-    elif q.index(t) > len(q) - q.index(t):
-        while True:
-            num = q.pop()
-            if num != t:
-                cnt += 1
-                q.appendleft(num)
-                
-            else:
-                cnt += 1
-                break
+    idx = q.index(t)
+    
+    if idx < len(q) - idx:
+       cnt += idx
+       q.rotate(-idx)
+
+    else:
+        cnt += len(q) - idx
+        q.rotate(len(q) - idx)
         
+    q.popleft()
+    
 print(cnt)

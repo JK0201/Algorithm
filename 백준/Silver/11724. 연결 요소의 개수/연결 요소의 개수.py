5350fc2cfd0947c23f7151344cc2ede07f7dda37
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-15 15:23:50 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 908 ms, Memory: 132876 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,6 +1,5 @@
-from collections import deque
-
 import sys
+sys.setrecursionlimit(10**6)
 n, m = map(int, sys.stdin.readline().split())
 data = []
 for _ in range(m):
@@ -15,21 +14,16 @@ for d in data:
     graph[d[0]-1].append(d[1]-1)
     graph[d[1]-1].append(d[0]-1)
 
-def bfs(v):
-    visited[v] = True
-    q = deque()
-    q.append(v)
+def dfs(cur_v):
+    visited[cur_v] = True
 
-    while q:
-        cur_v = q.popleft()
-        for v in graph[cur_v]:
-            if not visited[v]:
-                visited[v] = True
-                q.append(v)
+    for v in graph[cur_v]:
+        if not visited[v]:
+            dfs(v)
 
 for i in range(len(graph)):
     if not visited[i]:
-        bfs(i)
+        dfs(i)
         cnt += 1
 
 print(cnt)

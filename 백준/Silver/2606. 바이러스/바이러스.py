1ec7ecb1e2bf52a6aaa5f6af99c8a5d1ce38c4b5
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-17 15:40:24 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,31 +1,25 @@
 import sys
-from collections import deque
 
 data = list(map(str, sys.stdin.read().splitlines()))
 vertexs = int(data[0])
 edges = int(data[1])
 graph = [[] for _ in range(vertexs)]
 visited = [False] * vertexs
+cnt = 0
 
 for n in range(2, len(data)):
     a, b = map(int, data[n].split())
     graph[a-1].append(b-1)
     graph[b-1].append(a-1)
     
-def bfs(n):
-    cnt = 0
-    visited[n] = True
-    q = deque([n])
-    
-    while q:
-        cur_v = q.popleft()
+def dfs(cur_v):
+    global cnt
+    visited[cur_v] = True
         
-        for v in graph[cur_v]:
-            if not visited[v]:
-                cnt += 1
-                visited[v] = True
-                q.append(v) 
+    for v in graph[cur_v]:
+        if not visited[v]:
+            cnt += 1
+            dfs(v)
 
-    return cnt
-
-print(bfs(0))
+dfs(0)
+print(cnt)

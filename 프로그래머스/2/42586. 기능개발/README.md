17e19c7d13666e2b84c4b01c5269e20d8baa2f8e
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-08-20 19:28:41 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.05 ms
+메모리: 10.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 18일 17:29:28
+2024년 08월 20일 19:28:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,15 +2,16 @@ from collections import deque
 
 def solution(progresses, speeds):
     answer = []
-    q = deque(progresses)
+    p = deque(progresses)
     s = deque(speeds)
     
+    days = 0
     cnt = 0
-    time = 0
     
-    while q:
-        if q[0] + s[0] * time >= 100:
-            q.popleft()
+    while p:
+        cur_prog = p[0] + s[0] * days
+        if cur_prog >= 100:
+            p.popleft()
             s.popleft()
             cnt += 1
             
@@ -18,7 +19,8 @@ def solution(progresses, speeds):
             if cnt > 0:
                 answer.append(cnt)
                 cnt = 0
-            time += 1
-            
+                
+            days += 1
+    
     answer.append(cnt)
     return answer

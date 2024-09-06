abba85e3f9b6071da18f9f34ad692e4cdd17b8dc
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-09-06 12:19:48 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static LinkedHashMap<String, Integer> bookSet = new LinkedHashMap<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] title = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            title[i] = br.readLine();
+        }
+
+        Arrays.sort(title);
+
+        for (int i = 0; i < n; i++) {
+            if (!bookSet.containsKey(title[i])) {
+                bookSet.put(title[i], 1);
+            } else {
+                bookSet.put(title[i], bookSet.get(title[i]) + 1);
+            }
+        }
+
+        int bestSeller = Collections.max(bookSet.values());
+        
+        for (Map.Entry<String, Integer> entry : bookSet.entrySet()) {
+            if (entry.getValue() == bestSeller) {
+                System.out.println(entry.getKey());
+                break;
+            }
+        }
+    }
+}

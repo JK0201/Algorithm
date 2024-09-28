2d827ff95c2e671d0f38b84c9c8fd8132de0dbbf
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-09-28 13:15:54 +0900

    [Silver II] Title: 좌표 압축, Time: 1700 ms, Memory: 274324 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -12,21 +12,15 @@ public class Main {
         int[] nums = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < n; i++) nums[i] = Integer.parseInt(st.nextToken());
 
         int[] sorted_nums = Arrays.stream(nums).sorted().toArray();
 
         Map<Integer, Integer> memo = new HashMap<>();
         int rank = 0;
-        for (int num : sorted_nums) {
-            if (!memo.containsKey(num)) memo.put(num, rank++);
-        }
-
-        for (int num: nums) {
-            sb.append(memo.get(num)).append(' ');
-        }
+        for (int num : sorted_nums) if (!memo.containsKey(num)) memo.put(num, rank++);
+        
+        for (int num : nums) sb.append(memo.get(num)).append(' ');
 
         bw.write(sb.toString());
         bw.flush();

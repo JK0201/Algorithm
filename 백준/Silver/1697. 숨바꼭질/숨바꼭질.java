b7925d0db308e4cc1256cb63d8c8533aab66a7e8
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-10-01 11:10:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 19352 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited = new boolean[100001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(n, k));
+        br.close();
+    }
+
+    static int bfs(int start_v, int target_v) {
+        Queue<int[]> q = new LinkedList<>();
+        int min_cost = 0;
+        visited[start_v] = true;
+        q.offer(new int[]{0, start_v});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cur_cost = cur[0];
+            int cur_v = cur[1];
+
+            if (cur_v == target_v) {
+                min_cost = cur_cost;
+            }
+
+            for (int i = 0; i < 3; i++) {
+                int next_v;
+
+                if (i == 0) next_v = cur_v + 1;
+                else if (i == 1) next_v = cur_v - 1;
+                else next_v = cur_v * 2;
+
+                if (next_v >= 0 && next_v <= 100000 && !visited[next_v]) {
+                    visited[next_v] = true;
+                    q.offer(new int[]{cur_cost + 1, next_v});
+                }
+            }
+        }
+
+        return min_cost;
+    }
+}

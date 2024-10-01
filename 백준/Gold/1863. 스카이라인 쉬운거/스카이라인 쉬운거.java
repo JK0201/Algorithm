aae57b4162f7808b0b805865a63f404fb68ab0e8
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-10-01 14:59:26 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 184 ms, Memory: 19916 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            int y = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty() && stack.peek() > y) {
+                stack.pop();
+                count++;
+            }
+
+            if (stack.isEmpty() || stack.peek() < y) stack.push(y);
+        }
+
+        while (!stack.isEmpty()) {
+            if (stack.pop() > 0) count++;
+        }
+        
+        System.out.println(count);
+        br.close();
+    }
+}

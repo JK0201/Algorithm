e7571a8bda8528ee0968a776e12f8d604795e3dc
Justin Kim <124655981+JK0201@users.noreply.github.com>
2024-10-04 02:14:28 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Map<Integer, Long> memo = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(dfs(num)).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static Long dfs(int num) {
+        if (num == 0) return 0L;
+
+        if (num == 1 || num == 2) return 1L;
+
+        if (!memo.containsKey(num)) {
+            memo.put(num, dfs(num - 2) + dfs(num - 3));
+        }
+
+        return memo.get(num);
+    }
+}
